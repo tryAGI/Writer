7b0123cc4e8eeebd2a6bc62575ab71645e484a9c
Konstantin S. <havendv@gmail.com>
2026-04-06 19:53:49 +0400

    feat: Updated OpenAPI spec (#20)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Writer/Generated/Writer.JsonConverters.OneOf2.g.cs
+++ src/libs/Writer/Generated/Writer.JsonConverters.OneOf2.g.cs
@@ -3,7 +3,7 @@
 namespace Writer.JsonConverters
 {
     /// <inheritdoc />
-    public class OneOfJsonConverter<T1, T2> : global::System.Text.Json.Serialization.JsonConverter<global::Writer.OneOf<T1, T2>>
+    public class OneOfJsonConverter<[global::System.Diagnostics.CodeAnalysis.DynamicallyAccessedMembers(global::System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes.PublicProperties)] T1, [global::System.Diagnostics.CodeAnalysis.DynamicallyAccessedMembers(global::System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes.PublicProperties)] T2> : global::System.Text.Json.Serialization.JsonConverter<global::Writer.OneOf<T1, T2>>
     {
         /// <inheritdoc />
         public override global::Writer.OneOf<T1, T2> Read(

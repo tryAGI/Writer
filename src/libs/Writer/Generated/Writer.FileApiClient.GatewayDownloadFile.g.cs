2bc93e77b1fdb3aac2f0272dba9c0c58b3486519
Konstantin S. <havendv@gmail.com>
2026-05-24 08:00:50 +0400

    feat: Updated OpenAPI spec (#41)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Writer/Generated/Writer.FileApiClient.GatewayDownloadFile.g.cs
+++ src/libs/Writer/Generated/Writer.FileApiClient.GatewayDownloadFile.g.cs
@@ -362,17 +362,15 @@ namespace Writer
                                 {
                                 }
 
-                                throw new global::Writer.ApiException(
+                                throw global::Writer.ApiException.Create(
+                                    statusCode: __response.StatusCode,
                                     message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                     innerException: __ex,
-                                    statusCode: __response.StatusCode)
-                                {
-                                    ResponseBody = __content,
-                                    ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                    responseBody: __content,
+                                    responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                         __response.Headers,
                                         h => h.Key,
-                                        h => h.Value),
-                                };
+                                        h => h.Value));
                             }
 
                 }
@@ -681,16 +679,15 @@ namespace Writer
                                 }
                                 catch (global::System.Exception __ex)
                                 {
-                                    throw new global::Writer.ApiException(
+                                    throw global::Writer.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: null,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
                             else
@@ -725,17 +722,15 @@ namespace Writer
                                     {
                                     }
 
-                                    throw new global::Writer.ApiException(
+                                    throw global::Writer.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
 
